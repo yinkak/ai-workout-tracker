64a90090d205423c4ece46158a3947c0c3dd7d16
Yinka Kolawole <YK@MacBook-Pro-374.local>
2025-07-20 22:28:52 -0700

    updated requirements

--- app/pages/Workout_History.py
+++ app/pages/Workout_History.py
@@ -2,9 +2,9 @@
 
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 # Import only the Google Sheets connection and loading functions from utils.py
 # Make sure your src/utils.py contains these functions exactly as they were provided
